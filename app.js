df6514d19e24374d8ff034e7cb6ed6f0dbc6cc5a
LJ Kenward <ljkrooker@gmail.com>
2016-04-30 15:27:44 +1000

    add echo bot

--- app.js
+++ app.js
@@ -32,3 +32,22 @@ app.post('/webhook', function (req, res) {
     }
     res.sendStatus(200);
 });
+
+// generic function sending messages
+function sendMessage(recipientId, message) {
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
