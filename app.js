9367789a9ce6c80c2cc4be44ef22deb4413806fd
LJ Kenward <ljkrooker@gmail.com>
2016-05-05 17:37:57 +1000

    check logs

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.get('/webhook', function (req, res) {
 
 // handler receiving messages
 app.post('/webhook', function (req, res) {
+    console.log(req);
     var events = req.body.entry[0].messaging;
     for (i = 0; i < events.length; i++) {
         var event = events[i];
